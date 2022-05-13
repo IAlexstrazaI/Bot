39f68fbfb39967924ee4b634d3d00e32d9d04fd5
IAlexstrazaI <nekker1337@gmail.com>
2022-05-13 12:14:36 +0300

    терминал для отслеживания всех запросов + отработанна отправка пикч

--- bot.py
+++ bot.py
@@ -11,7 +11,16 @@ def start_message(message):
 
 @bot.message_handler(content_types=["text"])
 def start_message(message):
-  bot.send_message(message.chat.id, message.text)
+    print(message.text)
+
+    if (message.text.lower().find("нагаторо") != -1):
+        photo = open("nagatoro.jpg","rb")
+        bot.send_photo(message.chat.id,photo,caption="Нагаторо detected")
+
+    if (message.text.lower().find("лоли") != -1):
+        bot.send_photo(message.chat.id,'https://i1.sndcdn.com/artworks-000588837944-wshxw2-t500x500.jpg',caption="Лоли detected")
+
+    else:bot.send_message(message.chat.id, message.text)
 
 
 
